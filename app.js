cc3282e0deaa213a6db573e519d2e551ca66acd0
michaellambo <lambomichael7@gmail.com>
2023-12-20 15:27:53 -0600

    Full youtube clone

---
+++ app.js
@@ -0,0 +1,61 @@
+const videoCardContainer = document.querySelector('.video-container');
+
+let api_key = "AIzaSyD7ctF4X-mfqMgYo2lyiP3BUmNh479yl6U";
+let video_https = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_https = "https://www.googleapis.com/youtube/v3/channels?";
+
+fetch(video_https + new URLSearchParams({
+    key: api_key,
+    part: 'snippet',
+    chart: 'mostPopular',
+    maxResults: 50,
+    regionCode: 'US'
+}))
+.then(res => res.json())
+.then(data => {
+//    console.log(data);
+    data.items.forEach(item => {
+        getChannelIcon(item);
+    })
+})
+.catch(error => console.log(error));
+
+const getChannelIcon = (video_data) => {
+    fetch(channel_https + new URLSearchParams({
+        key: api_key,
+        part: 'snippet',
+        id: video_data.snippet.channelId
+    }))
+    .then(res => res.json())
+    .then(data => {
+        video_data.channelThumbnail = data.items[0].snippet.thumbnails.default.url;
+        makeVideoCard(video_data);
+    })
+}
+
+const makeVideoCard = (data) => {
+    videoCardContainer.innerHTML += `
+    <div class="video" onClick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+// search bar
+
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+    if(searchInput.value.length){
+        location.href = searchLink + searchInput.value;
+    }
+})
